3c70608aa727bf8f2b8ed7572f3a0552f8f89399
mtxjwo <jennifer@foregate.co>
2022-09-13 03:24:10 +0200

    Update; reg counter corrected with fix. Recount accounted for.

--- reg-count.js
+++ reg-count.js
@@ -13,7 +13,7 @@ let regCounter = 0;
 let currentTxToCheck = 1;
 let checkLock = false;
 setInterval(() => {
-  if (checkLock === true && currentTxToCheck >= txListLimit) {
+  if (checkLock === true) {
     return;
   }
   checkLock = true;
@@ -21,26 +21,32 @@ setInterval(() => {
   const txToCheck = dataPacket[0];
   const txType = dataPacket[dataPacket.length - 2];
   if (txType.indexOf("Request") > -1) {
+    checkLock = false;
     currentTxToCheck++;
     return;
   }
-  provider.getTransactionReceipt(txToCheck).then((r) => {
-    const qualifiedLogs = r.logs.filter((l) => {
-      return (
-        l.topics.includes(
-          "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
-        ) === true &&
-        l.topics[1] ===
-          "0x0000000000000000000000000000000000000000000000000000000000000000"
+  provider
+    .getTransactionReceipt(txToCheck)
+    .then((r) => {
+      const qualifiedLogs = r.logs.filter((l) => {
+        return (
+          l.topics.includes(
+            "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
+          ) === true &&
+          l.topics[1] ===
+            "0x0000000000000000000000000000000000000000000000000000000000000000"
+        );
+      });
+      regCounter += qualifiedLogs.length;
+      console.log("TX #", currentTxToCheck, "Registration Count:", regCounter);
+      fs.appendFileSync(
+        "txs-result.log",
+        "TX #" + currentTxToCheck + " Registration Count: " + regCounter + "\n"
       );
+      currentTxToCheck++;
+      checkLock = false;
+    })
+    .catch((e) => {
+      checkLock = false;
     });
-    regCounter += qualifiedLogs.length;
-    console.log("TX #", currentTxToCheck, "Registration Count:", regCounter);
-    fs.appendFileSync(
-      "txs-result.log",
-      "TX #" + currentTxToCheck + " Registration Count: " + regCounter + "\n"
-    );
-    currentTxToCheck++;
-    checkLock === false;
-  });
 }, 100);
